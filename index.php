50c525c99e03b13a31ab43528766f35735eccd94
en-OU <157330224+en-OU@users.noreply.github.com>
2024-10-07 13:35:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 $pageTitle = "Home";
-include "view-header.php";
 ?>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Newby Homework 1</title>
+    <title>Newby Homework 4</title>
     <link rel="icon" href="spacebook.jpg"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
