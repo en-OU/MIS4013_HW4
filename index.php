0289196e19b864b11988a3824412cdd13860f057
en-OU <157330224+en-OU@users.noreply.github.com>
2024-10-09 13:36:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,6 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 
-<?php
-function get_db_connection(){
-
-    $conn = new mysqli(hw2.mysql.database.azure.com, D3misres, ILL92@41, 4013hw3db);
-    
-    if ($conn->connect_error) {
-      return false;
-    }
-    return $conn;
-}
-?>
-
 <span style="color: violet; font-size: 20px; display: inline-block;">Home</span>
 </br>
 <span style="color: violet; font-size: 150px; display: inline-block;">MIS 4013 HW4 - Emily Newby</span>
