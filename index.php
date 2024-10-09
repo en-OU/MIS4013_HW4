6938728fa6202a5724825041a9666007e5150e1e
en-OU <157330224+en-OU@users.noreply.github.com>
2024-10-09 13:20:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Newby Homework 4</title>
-    <link rel="icon" href="spacebook.jpg"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<span style="color: violet; font-size: 20px; display: inline-block;">MIS 4013 HW2 - Emily Newby</span>
+<span style="color: violet; font-size: 20px; display: inline-block;">Home</span>
+</br>
+<span style="color: violet; font-size: 150px; display: inline-block;">MIS 4013 HW4 - Emily Newby</span>
 
